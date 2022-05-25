f36b82712f072bdc913579b730277a22d8bcada0
calmkeen <iyunseong159@gmail.com>
2022-05-25 17:22:14 +0900

    shareSpot

---
+++ SpotShare/SpotShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotShare
+//
+//  Created by calmkeen on 2022/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

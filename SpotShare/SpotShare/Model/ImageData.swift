f0524226bd6c113b7e9762c3746bab1902cc2884
calmkeen <iyunseong159@gmail.com>
2022-05-27 17:14:40 +0900

    카메라 앨범 접근 기능 구현

---
+++ SpotShare/SpotShare/Model/ImageData.swift
@@ -0,0 +1,20 @@
+//
+//  ImageData.swift
+//  SpotShare
+//
+//  Created by calmkeen on 2022/05/27.
+//
+
+import Foundation
+import UIKit
+
+
+struct ImageData{
+    var title: String = ""
+    var image: UIImage
+    var location: String = ""
+    var date: Date
+
+    }
+    
+

236418f0a58c7105b906dfe139aaa44da5778ae8
calmkeen <iyunseong159@gmail.com>
2022-06-20 17:06:31 +0900

    프로젝트 규모가 너무 크게 잡아서 핵심 기능을 proto type으로 계산 후  작업

---
+++ mySpot/mySpot/mySpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mySpot
+//
+//  Created by calmkeen on 2022/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
